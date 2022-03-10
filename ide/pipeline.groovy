edc8bf24221c98e4d51e821d727b63171f5bca33
Igor Antonov <5915087@ups.com>
2022-03-10 06:36:41 -0500

    some pipeline changes

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -4,13 +4,13 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
+                // Get some code from a GitHub repository
+                git branch: 'main', url: 'https://github.com/intercreativer/jgsu-spring-petclinic.git'  
             }            
         }
         stage('Build') {
             steps {
-                sh './mvnw clean package'
-                //sh 'false' // true
+                bat 'mvnw.cmd clean package'
             }
         
             post {
@@ -18,14 +18,14 @@ pipeline {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
                 }
-                changed {
-                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
-                        body: "Please go to ${BUILD_URL} and verify the build", 
-                        attachLog: true, 
-                        compressLog: true, 
-                        to: "test@jenkins",
-                        recipientProviders: [upstreamDevelopers(), requestor()]
-                }
+                // changed {
+                //     emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
+                //         body: "Please go to ${BUILD_URL} and verify the build", 
+                //         attachLog: true, 
+                //         compressLog: true, 
+                //         to: "test@jenkins",
+                //         recipientProviders: [upstreamDevelopers(), requestor()]
+                // }
             }
         }
     }
